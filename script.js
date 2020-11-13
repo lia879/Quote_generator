1e20a303d31aeeb4ab0aec9d4e2b4dba095a843c
Lia879 <54521457+lia879@users.noreply.github.com>
2020-11-12 20:10:07 -0600

    quote generator update

---
+++ script.js
@@ -0,0 +1,45 @@
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
+
+// get quotes from API 
+
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        //if Author is blank, add 'unknown'; 
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quote 
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+    } catch (error) {
+        getQuote();
+    }
+    {
+
+    }
+
+
+
+
+}
+
+// on lead 
+getQuote();
